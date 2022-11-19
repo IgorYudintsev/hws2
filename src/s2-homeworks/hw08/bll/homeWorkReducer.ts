cb3d2109764c0dee49e2e1b418441581eba9cf18
IgorYudintsev <kakvospitat@gmail.com>
2022-11-19 12:38:06 +0300

    8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,51 +1,51 @@
-// import {UserType} from '../HW8'
-//
-// type ActionType =
-//     | { type: 'sort'; payload: 'up' | 'down' }
-//     | { type: 'check'; payload: number }
-//
-// export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
-//     switch (action.type) {
-//         case 'sort': { // by name
-//             console.log('sort')
-//             if(action.payload==='up'){
-//                 return state.sort((a,b)=> a.name > b.name? 1:-1)
-//             }else{
-//                 return state.sort((a,b)=> a.name < b.name? 1:-1)
-//             }
-//         }
-//         case 'check': {
-//             return state.filter((a) => a.age >= action.payload)
-//         }
-//         default:
-//             return state
-//     }
-// }
-
-//--------------------------------------------------
-
 import {UserType} from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        // делают студенты
-        case 'sort': { // by name
-            const newState = [...state].sort((a, b) => {
-                if (a.name > b.name) return 1
-                else if (a.name < b.name) return -1
-                else return 0
-            })
-
-            return action.payload === 'up' ? newState : newState.reverse()
+         case 'sort': { // by name
+           let copyState=[...state]
+            if(action.payload==='up'){
+                return copyState.sort((a,b)=> a.name > b.name? 1:-1)
+            }else{
+                return copyState.sort((a,b)=> a.name < b.name? 1:-1)
+            }
         }
         case 'check': {
-            return state.filter((a: any) => a.age >= action.payload)
+            return state.filter((a) => a.age >= action.payload)
         }
         default:
             return state
     }
 }
+
+//--------------------------------------------------
+
+// import {UserType} from '../HW8'
+//
+// type ActionType =
+//     | { type: 'sort'; payload: 'up' | 'down' }
+//     | { type: 'check'; payload: number }
+//
+// export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+//     switch (action.type) {
+//         // делают студенты
+//         case 'sort': { // by name
+//             const newState = [...state].sort((a, b) => {
+//                 if (a.name > b.name) return 1
+//                 else if (a.name < b.name) return -1
+//                 else return 0
+//             })
+//
+//             return action.payload === 'up' ? newState : newState.reverse()
+//         }
+//         case 'check': {
+//             return state.filter((a: any) => a.age >= action.payload)
+//         }
+//         default:
+//             return state
+//     }
+// }
